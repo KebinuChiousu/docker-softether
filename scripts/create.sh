f464b2b822e0a81d1f9fa39ef19c25e242b55b13
Kevin Meredith <meredithk@tron.evolutionarynetworkingdesigns.net>
2016-11-08 20:18:54 -0800

    Fix permissions on creation of initial files

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,5 +1,7 @@
-mkdir -p /opt/softether
-touch /opt/softether/vpn_server.config
+sudo mkdir -p /opt/softether
+sudo touch /opt/softether/vpn_server.config
+sudo chown -R docker:docker /opt/softether
+sudo chown 775 /opt/softether
 docker volume create --name softether-logs
 docker run -d -v /opt/softether/vpn_server.config:/usr/local/vpnserver/vpn_server.config \
               -v softether-logs:/var/log/vpnserver --net host --cap-add NET_ADMIN \
